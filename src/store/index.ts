19fa03e7019bb42cee81c540c212d967d32da14b
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-06-20 00:50:45 +0800

    Export new store functions

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
+export * from './deduce.js';
 export * from './IReadableStore.js';
 export * from './IWritableStore.js';
 export * from './Store.js';
 export * from './Supply.js';
+export * from './use.js';
