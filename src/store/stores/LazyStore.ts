55ec107b137069b84ef91ff7beb12fbd3cf1ec36
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-09-02 02:35:49 +0800

    Add `LazyStore`

---
+++ src/store/stores/LazyStore.ts
@@ -0,0 +1,30 @@
+import { lazy } from '../../functional/lazy.js';
+import { type StartStopNotifier } from '../ReadableStore.js';
+import { Store } from '../Store.js';
+
+/**
+ * Store that only runs `factory` to initialise itself when a consumer requires
+ * it
+ */
+export class LazyStore<T = unknown> extends Store<T> {
+	private initialised = false;
+	private lazy: ReturnType<typeof lazy>;
+
+	constructor(factory: () => T, onStarted?: StartStopNotifier<T>) {
+		super(undefined as never, onStarted);
+
+		this.lazy = lazy(factory);
+	}
+
+	public override get() {
+		if (!this.initialised) return this.lazy() as T;
+
+		return super.get();
+	}
+
+	public override set(v: T) {
+		this.initialised ??= true;
+
+		super.set(v);
+	}
+}
--- src/store/stores/index.ts
+++ src/store/stores/index.ts
@@ -3,3 +3,4 @@ export * from './ExtensibleStore.js';
 export * from './SetStore.js';
 export * from './ShapedArrayStore.js';
 export * from './WeakSetStore.js';
+export * from './LazyStore.js';
