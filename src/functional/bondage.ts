0e905d23e09fb1fed8ec9544d503044f738855c7
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-12 20:27:57 +0800

    Switch to static strategy for `bondage`

--- src/functional/bondage.ts
+++ src/functional/bondage.ts
@@ -1,16 +1,9 @@
-export const bondage = <T extends Record<any, any>>(obj: T) => {
-	return new Proxy(
-		obj,
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-		Object.assign(Object.create(Reflect), {
-			get(target, prop, receiver) {
-				const value = Reflect.get(target, prop, receiver);
-				if (typeof value === 'function')
-					return (value as () => any).bind(target);
-
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-				return value;
-			},
-		} satisfies ProxyHandler<T>),
-	);
-};
+export const bondage = <T extends Record<any, any>>(obj: T) =>
+	Object.assign(
+		Object.create(obj),
+		Object.fromEntries(
+			Object.entries(obj)
+				.filter(([key]) => typeof obj[key] === 'function')
+				.map(([key, fn]) => [key, (fn as () => void).bind(obj)]),
+		),
+	) as T;
