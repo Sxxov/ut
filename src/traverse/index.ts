c1b85179bf42cc29a6fdc2fd17b88f6506893407
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-17 02:06:10 +0800

    Expose new traversers

--- src/traverse/index.ts
+++ src/traverse/index.ts
@@ -2,5 +2,7 @@ export * from './traverseLeaves.js';
 export * from './traverseLeavesAssign.js';
 export * from './traverseLeavesCompare.js';
 export * from './traverseLeavesMap.js';
+export * from './traverseLeavesObjectMap.js';
 export * from './traverseLeavesPair.js';
 export * from './traverseProperty.js';
+export * from './traversePropertyMap.js';
