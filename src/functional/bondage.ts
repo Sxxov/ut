cd9ed02c79b7c432fd2dd5cc0ce4267c2838d773
jason lim <50891910+Sxxov@users.noreply.github.com>
2023-10-29 17:54:34 +0800

    Make `bondage` walk up prototype

--- src/functional/bondage.ts
+++ src/functional/bondage.ts
@@ -1,9 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+const getAllPropertyNames = (obj: any) => {
+	const props = new Set<string>();
+
+	let curr = obj;
+	do {
+		for (const prop of Object.getOwnPropertyNames(obj)) props.add(prop);
+	} while ((curr = Object.getPrototypeOf(curr)) && obj !== Object.prototype);
+
+	return [...props];
+};
+/* eslint-enable */
+
 export const bondage = <T extends Record<any, any>>(obj: T) =>
 	Object.assign(
 		Object.create(obj),
 		Object.fromEntries(
-			Object.entries(obj)
-				.filter(([key]) => typeof obj[key] === 'function')
-				.map(([key, fn]) => [key, (fn as () => void).bind(obj)]),
+			getAllPropertyNames(obj)
+				.filter((key) => typeof obj[key] === 'function')
+				.map((key) => [key, (obj[key] as () => void).bind(obj)]),
 		),
 	) as T;
